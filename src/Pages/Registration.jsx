73a1cae230383c3b2f80e0ac0a90ccb67b377df0
Homelander-69 <abhinashpaul370@gmail.com>
2023-05-29 22:05:40 +0600

    loading when account created

--- src/Pages/Registration.jsx
+++ src/Pages/Registration.jsx
@@ -1,17 +1,22 @@
 import React, { useState } from "react";
 import { Grid, TextField, Button } from "@mui/material";
+import LoadingButton from "@mui/lab/LoadingButton";
 import regiimg from "../assets/Registration-img.webp";
 import Regiheadinglog from "../Components/Regiheadinglog";
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { Navigate, useNavigate } from "react-router-dom";
 
 let intialValues = {
    email: "",
    fullname: "",
    password: "",
+   loading: false,
 };
 
 const Registration = () => {
    const auth = getAuth();
+
+   let navigate = useNavigate();
 
    let [values, setValues] = useState(intialValues);
 
@@ -24,8 +29,20 @@ const Registration = () => {
 
    let handleSubmit = () => {
       let { email, fullname, password } = values;
+      setValues({
+         ...values,
+         loading: true,
+      });
       createUserWithEmailAndPassword(auth, email, password).then((user) => {
          console.log(user);
+         setValues({
+            ...values,
+            email: "",
+            fullname: "",
+            password: "",
+            loading: false,
+         });
+         navigate("/login");
       });
    };
    return (
@@ -40,6 +57,8 @@ const Registration = () => {
                <div className="reg-form">
                   <TextField
                      onChange={handleValues}
+                     type="email"
+                     value={values.email}
                      name="email"
                      id="outlined-basic"
                      label="Email-Adress"
@@ -49,6 +68,7 @@ const Registration = () => {
                <div className="reg-form">
                   <TextField
                      onChange={handleValues}
+                     value={values.fullname}
                      name="fullname"
                      id="outlined-basic"
                      label="First-Name"
@@ -58,15 +78,23 @@ const Registration = () => {
                <div className="reg-form">
                   <TextField
                      onChange={handleValues}
+                     type="password"
+                     value={values.password}
                      name="password"
                      id="outlined-basic"
                      label="Password"
                      variant="outlined"
                   />
                </div>
-               <Button onClick={handleSubmit} variant="contained">
-                  Sign Up
-               </Button>
+               {values.loading ? (
+                  <LoadingButton loading variant="outlined">
+                     Submit
+                  </LoadingButton>
+               ) : (
+                  <Button onClick={handleSubmit} variant="contained">
+                     Sign Up
+                  </Button>
+               )}
             </div>
          </Grid>
          <Grid item xs={6}>
